52c96fe34d97b7bdae4b40d253c2aa28738e5453
Artur Barilo <efreyu@gmail.com>
2024-07-16 20:32:03 +0300

    master | Add configs for custom godot build configuration and general project settings

---
+++ tools/config/custom_build.py
@@ -0,0 +1,7 @@
+# Generated using https://godot-build-options-generator.github.io
+
+module_mobile_vr_enabled = "no"
+module_openxr_enabled = "no"
+module_webxr_enabled = "no"
+module_gdnative_enabled = "no"
+module_visual_script_enabled = "no"
